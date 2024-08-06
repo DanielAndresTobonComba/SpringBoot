3be2d0eb850106f89681d5d27584fe7c429b6f5e
Daniel Andres Tobon Comba <toboncombadanielandres@gmail.com>
2024-08-06 17:06:16 -0500

    dia2 falta como ver los datos por la url

--- app-j3/src/main/java/com/appdemoj3/app_j3/controllers/UserController.java
+++ app-j3/src/main/java/com/appdemoj3/app_j3/controllers/UserController.java
@@ -1,9 +1,14 @@
 package com.appdemoj3.app_j3.controllers;
 
 import java.util.Map;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.appdemoj3.app_j3.models.User;
@@ -24,7 +29,7 @@ public class UserController {
 
     // MODEL ES UNA ESTRUCTURA DE DATOS PARA PODER SER LLAMADAO Y CREAR ACCESO Y SALIDA A ESOS DATOS
 
-    @GetMapping("/details")
+/*     @GetMapping("/details")
     public String details(Model model){
 
     User user = new User("Johlver","Pardo");
@@ -33,6 +38,33 @@ public class UserController {
     model.addAttribute("user", user);
     return "details";
   
+    } */
+
+
+    // MAPEAR CON LOS IF
+    @GetMapping("/details")
+    public String details(Model model){
+    User user = new User();
+    user.setNombre("Daniel Andres");
+    user.setApellido("Tobon Comba");
+    //user.setEmail("jjpardo2002@gmail.com"); //Email del usuario
+    model.addAttribute("title", "Desarrollando con Spring boot CreativeCode");
+    model.addAttribute("user", user);
+    return "details"; 
+}
+
+
+    @GetMapping("/list")
+    public String list(ModelMap model){
+        List<User> users = Arrays.asList(
+        new User("Carlos", "Perez" , null),
+        new User("Martha","Sanchez" , "martha@gmail.com"),
+        new User("Vicente","Camargo" , "camargo@gmal.com")
+        );
+        model.addAttribute("title", "Listado de Usuarios");
+        model.addAttribute("users", users);
+        return "list";
     }
+
 
 }
